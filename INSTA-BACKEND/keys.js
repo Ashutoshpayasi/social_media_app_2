82d83adc2d3f23bf06b7623e8666bb3ccbab8ad7
Ashutoshpayasi <asutoshpayasi2001@gmail.com>
2023-05-31 22:41:39 +0530

    initial commit

---
+++ INSTA-BACKEND/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGO_URI:"mongodb+srv://asutoshpayasi2001:MqXCBSHt9JEonecb@cluster0.beye70l.mongodb.net/Instagram?retryWrites=true&w=majority",
+    JWT_SECRET_TOKEN:"fjak897432"
+}
